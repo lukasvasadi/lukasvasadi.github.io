3788a0a1d973b1da1e559f9543b0b351e3123cca
lukasvasadi <lukas.vasadi@gmail.com>
2021-10-03 09:13:44 -0400

    remove duplicates

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var links = document.getElementById('mobile-menu');
-
-function menuDropdown() {
-    if (links.style.visibility === 'visible') {
-        links.style.visibility = 'hidden';
-    } else {
-        links.style.visibility = 'visible';
-    }
-}
