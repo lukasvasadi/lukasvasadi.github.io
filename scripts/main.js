b409fc02360b8c141bac8ae83bf755f9afd713b7
lukasvasadi <lukas.vasadi@gmail.com>
2022-04-26 18:58:18 -0400

    reset mobile menu icon background

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ var links = document.getElementById('mobile-menu');
 function menuDropdown() {
     if (links.style.visibility === 'visible') {
         links.style.visibility = 'hidden';
+        links.style.background = '#222';
     } else {
         links.style.visibility = 'visible';
     }
